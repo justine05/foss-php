e08b1c16d44201e01354da4983cdb74437063a76
Justine Biju <36364765+justine05@users.noreply.github.com>
2019-05-05 08:11:09 +0400

    First Commit

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+
+    $db = mysqli_connect("localhost","root","","foss_lab");
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $dbpass = mysqli_query($db,"SELECT password FROM users WHERE username = '$username' ");
+    
+    $p = mysqli_fetch_array($dbpass)["password"];
+    echo $p;
+
+    if($p === ""){
+        echo "Username does not exists";
+    }
+    else {
+        if($p === $password){
+            echo "Success";
+        } 
+    }
+
+?>
